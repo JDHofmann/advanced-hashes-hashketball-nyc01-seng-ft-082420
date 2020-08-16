d0fca1b94ba24dfb1836d23e6821ad8f827ec27c
JD Brewer Hofmann <justindhofmann@gmail.com>
2020-08-16 21:37:00 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -138,7 +138,7 @@ def player_stats(player_name)
 end
 
 def num_points_scored(player_name)
-  binding.pry
+  # binding.pry
   player_stats(player_name)[:points]
   #that takes in an argument of a player's
     #name and returns the number of points scored for that player
